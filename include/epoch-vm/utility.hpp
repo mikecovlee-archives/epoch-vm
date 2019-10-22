4d147eabc25da57a46e72cd39f035bd79302e3b7
Michael Lee <mikecovlee@163.com>
2019-10-22 16:03:20 +0800

    update

--- include/epoch-vm/utility.hpp
+++ include/epoch-vm/utility.hpp
@@ -112,6 +112,20 @@ namespace epoch {
 			return end - begin;
 		}
 	};
+
+	using byte_t=uint8_t;
+	using size_t=std::size_t;
+	template<typename data_t, typename size_t>
+	void memcpy_n(data_t *src, data_t *dest, size_t count)
+	{
+		if (count > 0) {
+			byte_t *_src = reinterpret_cast<byte_t *>(src);
+			byte_t *_dest = reinterpret_cast<byte_t *>(dest);
+			size_t _count = count * sizeof(data_t);
+			for (size_t idx = 0; idx < _count; ++idx)
+				*(_dest + idx) = *(_src + idx);
+		}
+	}
 } // namespace epoch
 
 /*
--- include/epoch-vm/vm.hpp
+++ include/epoch-vm/vm.hpp
@@ -12,17 +12,15 @@ namespace epoch {
 		using vm_char = signed char;
 		using vm_string = std::string;
 	}
-	using byte_t=uint8_t;
-	using size_t=std::size_t;
-	template<typename data_t, typename size_t>
-	void memcpy_n(data_t *src, data_t *dest, size_t count)
+	class instrument_base 
 	{
-		if (count > 0) {
-			byte_t *_src = reinterpret_cast<byte_t *>(src);
-			byte_t *_dest = reinterpret_cast<byte_t *>(dest);
-			size_t _count = count * sizeof(data_t);
-			for (size_t idx = 0; idx < _count; ++idx)
-				*(_dest + idx) = *(_src + idx);
-		}
-	}
+	public:
+		instrument_base() = default;
+		instrument_base(const instrument_base &) = default;
+		virtual ~instrument_base() = default;
+		virtual void exec() = 0;
+	};
+	class vm_instance final {
+		
+	};
 } // namespace epoch
